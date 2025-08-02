6d971682de83b3d71ae1ecfdf0ae95755451858c
zyexro <zyexro@proton.me>
2025-08-02 14:46:28 +0100

    anykernel.sh:kernel.string

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string= kernel by @zyexro
 do.devicecheck=1
 do.modules=0
 do.systemless=1
