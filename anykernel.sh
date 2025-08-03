af2dff4813742681d5f2dc79f5f9bfe1f9fc5fde
zyexro <zyexro@proton.me>
2025-08-03 14:04:18 +0100

    anykernel.sh:BLOCK: correct dir

--- anykernel.sh
+++ anykernel.sh
@@ -25,7 +25,7 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 } # end attributes
 
 # boot shell variables
-BLOCK=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+BLOCK=/dev/block/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
 PATCH_VBMETA_FLAG=auto;
